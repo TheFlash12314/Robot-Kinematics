c288f8b9c503f583c93f1b5b160ad3ef695a1ea9
TheFlash12314 <36818971+TheFlash12314@users.noreply.github.com>
2020-05-12 13:20:05 +0800

    Add files via upload

---
+++ Matrix.cpp
@@ -0,0 +1,190 @@
+/* Matrix.h
+  
+  Calculations of matrix values.
+  This code is created by TheFLash/YWW.
+*/
+#include "Matrix.h"
+#include <math.h>
+
+void invtran(float* Titi, float* Titf)
+{
+  // finding the inverse of the homogeneous transformation matrix
+  // first row
+  Titf[0*4 + 0] = Titi[0*4 + 0];
+  Titf[0*4 + 1] = Titi[1*4 + 0];
+  Titf[0*4 + 2] = Titi[2*4 + 0];
+  Titf[0*4 + 3] = -Titi[0*4 + 0]*Titi[0*4 + 3]-Titi[1*4 + 0]*Titi[1*4 + 3]-Titi[2*4 + 0]*Titi[2*4 + 3];
+  // second row
+  Titf[1*4 + 0] = Titi[0*4 + 1];
+  Titf[1*4 + 1] = Titi[1*4 + 1];
+  Titf[1*4 + 2] = Titi[2*4 + 1];
+  Titf[1*4 + 3] = -Titi[0*4 + 1]*Titi[0*4 + 3]-Titi[1*4 + 1]*Titi[1*4 + 3]-Titi[2*4 + 1]*Titi[2*4 + 3];
+  // third row
+  Titf[2*4 + 0] = Titi[0*4 + 2];
+  Titf[2*4 + 1] = Titi[1*4 + 2];
+  Titf[2*4 + 2] = Titi[2*4 + 2];
+  Titf[2*4 + 3] = -Titi[0*4 + 2]*Titi[0*4 + 3]-Titi[1*4 + 2]*Titi[1*4 + 3]-Titi[2*4 + 2]*Titi[2*4 + 3];
+  // forth row
+  Titf[3*4 + 0] = 0.0;
+  Titf[3*4 + 1] = 0.0;
+  Titf[3*4 + 2] = 0.0;
+  Titf[3*4 + 3] = 1.0;
+}
+
+void tran2pos(float* Ttp, float* Xtp)
+{
+  // pos from homogeneous transformation matrix
+  Xtp[0] = Ttp[0*4 + 3];
+  Xtp[1] = Ttp[1*4 + 3];
+  Xtp[2] = Ttp[2*4 + 3];
+  Xtp[4] = atan2(sqrt(Ttp[2*4 + 0]*Ttp[2*4 + 0] + Ttp[2*4 + 1]*Ttp[2*4 + 1]),Ttp[2*4 + 2]);
+  Xtp[3] = atan2(Ttp[1*4 + 2]/sin(Xtp[4]),Ttp[0*4 + 2]/sin(Xtp[4]));
+  Xtp[5] = atan2(Ttp[2*4 + 1]/sin(Xtp[4]),-Ttp[2*4 + 0]/sin(Xtp[4]));
+}
+
+void pos2tran(float* Xpt, float* Tpt)
+{
+  // pos to homogeneous transformation matrix
+  // first row
+  Tpt[0*4 + 0] = cos(Xpt[3])*cos(Xpt[4])*cos(Xpt[5])-sin(Xpt[3])*sin(Xpt[5]);
+  Tpt[0*4 + 1] = -cos(Xpt[3])*cos(Xpt[4])*sin(Xpt[5])-sin(Xpt[3])*cos(Xpt[5]);
+  Tpt[0*4 + 2] = cos(Xpt[3])*sin(Xpt[4]);
+  Tpt[0*4 + 3] = Xpt[0];
+  // second row
+  Tpt[1*4 + 0] = sin(Xpt[3])*cos(Xpt[4])*cos(Xpt[5])+cos(Xpt[3])*sin(Xpt[5]);
+  Tpt[1*4 + 1] = -sin(Xpt[3])*cos(Xpt[4])*sin(Xpt[5])+cos(Xpt[3])*cos(Xpt[5]);
+  Tpt[1*4 + 2] = sin(Xpt[3])*sin(Xpt[4]);
+  Tpt[1*4 + 3] = Xpt[1];
+  // third row
+  Tpt[2*4 + 0] = -sin(Xpt[4])*cos(Xpt[5]);
+  Tpt[2*4 + 1] = sin(Xpt[4])*sin(Xpt[5]);
+  Tpt[2*4 + 2] = cos(Xpt[4]);
+  Tpt[2*4 + 3] = Xpt[2];
+  // forth row
+  Tpt[3*4 + 0] = 0.0;
+  Tpt[3*4 + 1] = 0.0;
+  Tpt[3*4 + 2] = 0.0;
+  Tpt[3*4 + 3] = 1.0;
+}
+
+void DH1line(float thetadh, float alfadh, float rdh, float ddh, float* Tdh)
+{
+  // creats Denavit-Hartenberg homogeneous transformation matrix
+  // first row
+  Tdh[0*4 + 0] = cos(thetadh);
+  Tdh[0*4 + 1] = -sin(thetadh)*cos(alfadh);
+  Tdh[0*4 + 2] = sin(thetadh)*sin(alfadh);
+  Tdh[0*4 + 3] = rdh*cos(thetadh);
+  // second row
+  Tdh[1*4 + 0] = sin(thetadh);
+  Tdh[1*4 + 1] = cos(thetadh)*cos(alfadh);
+  Tdh[1*4 + 2] = -cos(thetadh)*sin(alfadh);
+  Tdh[1*4 + 3] = rdh*sin(thetadh);
+  // third row
+  Tdh[2*4 + 0] = 0.0;
+  Tdh[2*4 + 1] = sin(alfadh);
+  Tdh[2*4 + 2] = cos(alfadh);
+  Tdh[2*4 + 3] = ddh;
+  // forth row
+  Tdh[3*4 + 0] = 0.0;
+  Tdh[3*4 + 1] = 0.0;
+  Tdh[3*4 + 2] = 0.0;
+  Tdh[3*4 + 3] = 1.0;
+}
+/*void MatrixPrint(float* A, int m, int n, String label)
+{
+  // A = input matrix (m x n)
+  int i, j;
+  Serial.println();
+  Serial.println(label);
+  for (i = 0; i < m; i++)
+  {
+    for (j = 0; j < n; j++)
+    {
+      Serial.print(A[n * i + j]);
+      Serial.print("\t");
+    }
+    Serial.println();
+  }
+}*/
+
+void MatrixCopy(float* A, int n, int m, float* B)
+{
+  int i, j;
+  for (i = 0; i < m; i++)
+    for(j = 0; j < n; j++)
+    {
+      B[n * i + j] = A[n * i + j];
+    }
+}
+
+//Matrix Multiplication
+// C = A*B
+void MatrixMultiply(float* A, float* B, int m, int p, int n, float* C)
+{
+  // A = input matrix (m x p)
+  // B = input matrix (p x n)
+  // m = number of rows in A
+  // p = number of columns in A = number of rows in B
+  // n = number of columns in B
+  // C = output matrix = A*B (m x n)
+  int i, j, k;
+  for (i = 0; i < m; i++)
+    for(j = 0; j < n; j++)
+    {
+      C[n * i + j] = 0;
+      for (k = 0; k < p; k++)
+        C[n * i + j] = C[n * i + j] + A[p * i + k] * B[n * k + j];
+    }
+}
+
+
+//Matrix Addition
+void MatrixAdd(float* A, float* B, int m, int n, float* C)
+{
+  // A = input matrix (m x n)
+  // B = input matrix (m x n)
+  // m = number of rows in A = number of rows in B
+  // n = number of columns in A = number of columns in B
+  // C = output matrix = A+B (m x n)
+  int i, j;
+  for (i = 0; i < m; i++)
+    for(j = 0; j < n; j++)
+      C[n * i + j] = A[n * i + j] + B[n * i + j];
+}
+
+
+//Matrix Subtraction
+void MatrixSubtract(float* A, float* B, int m, int n, float* C)
+{
+  // A = input matrix (m x n)
+  // B = input matrix (m x n)
+  // m = number of rows in A = number of rows in B
+  // n = number of columns in A = number of columns in B
+  // C = output matrix = A-B (m x n)
+  int i, j;
+  for (i = 0; i < m; i++)
+    for(j = 0; j < n; j++)
+      C[n * i + j] = A[n * i + j] - B[n * i + j];
+}
+
+
+//Matrix Transpose
+void MatrixTranspose(float* A, int m, int n, float* C)
+{
+  // A = input matrix (m x n)
+  // m = number of rows in A
+  // n = number of columns in A
+  // C = output matrix = the transpose of A (n x m)
+  int i, j;
+  for (i = 0; i < m; i++)
+    for(j = 0; j < n; j++)
+      C[m * j + i] = A[n * i + j];
+}
+
+void MatrixScale(float* A, int m, int n, float k)
+{
+  for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++)
+      A[n * i + j] = A[n * i + j] * k;
+}
